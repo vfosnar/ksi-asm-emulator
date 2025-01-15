8a0f94bc456208aaadccc2730837653d5ef76780
Ninjaondra321 <ondran321@gmail.com>
2025-01-15 12:03:43 +0100

    Aaaa

--- ass-to-bytes.py
+++ ass-to-bytes.py
@@ -6,22 +6,37 @@ def convert_to_instructions(line: str) -> list[int]:  # List bajtů
     line = line.split(";", 1)[0]
     is_first_char = line[0].isalpha()
 
-    things = line.split()
+    parts = line.split()
 
     if is_first_char:
-        things.pop(0)
+        parts.pop(0)
 
-    match things[0]:
+    match parts[0]:
         case "NOP":
             return [0x90]
         case "MOV":
-            return convert_ADD(*things[1:])
+            return convert_ADD(*parts[1:])
 
-    assert False, f"Instrukce {things[0]} není implementována"
+    assert False, f"Instrukce {parts[0]} není implementována"
 
 
-def convert_ADD(p1, p2):
-    return ["Tady bude add"]
+def zpracuj_dalsi_instrukci():
+    # 1) Načíst byte.
+    #    - Pokud je prefix, načti další a pamatuj si ho
+    # 2) Zpracuj opcode - dešifruj, k čemu patří
+    # (early exit, pokud nepotřebuji ModR/M)
+    # 3) Zpracuj ModR/M, pokud ho potřebuju
+    # 4) Pokud potřebuji další data, tak si je načtu (poznám podle R/M bitů)¨
+    # Mám to ;-)
+
+    ...
+
+
+class Instruction:
+    def __init__(self):
+        self.opcode = 9
+        self.prefix = None  # | "DS" | "CS" | ...
+        self.modrm = None  # |
 
 
 if __name__ == "__main__":
