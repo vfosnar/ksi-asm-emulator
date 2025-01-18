9c1ea06687c5450e0fb6df3fa93ad27274da4ba3
Ondra N <ondran321@gmail.com>
2025-01-18 19:08:22 +0100

    Making repo public (jen načas)

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Ahoj, díky že jsi mě otevřel.
+
+Hotový parciální disassembler a náznak assembleru je v souboru disassembler.py. V emulátor pak najdete -nečekaně- v souboru emulator_01.py.
+
+Budu rád za jakoukoli pomoc nebo zpětnou vazbu.
+
+Varování: kód není čitelný
+
+PS: Pokud nejsi ORG této soutěže, prosím odejdi ;-)
+
--- disassembler.py
+++ disassembler.py
@@ -1,57 +1,5 @@
 
 
-
-def parse_param(p: str) -> 'Parameter': # Part of assembler
-    """
-    ! Work in progress. Yet can't handle labels nor math (MOV [label + 2], 42)
-    ? Perhaps regex would be usefull??
-    pls help
-    """
-    # Register
-    if p in REGISTERS:
-        return Register(p)
-
-    # Immutable
-    try:
-        print(parse_number(p))
-        return Immutable(parse_number(p))
-    except:
-        pass
-
-    # Memmory
-    if p[0] == '[' and p[-1] == "]":
-        p = p[1:-1]
-        a = Memmory(None, None)
-        for r in MOD_00_RM:
-            if r in p:
-                a.source = r
-                p = p.replace(r + '+', "+")
-                break
-        else:
-            raise Exception("Nebyl rozpoznán segment")
-        
-        if p[0] == "+":
-            p = p.replace('+', '')
-            a.displacement = parse_number(p) # If is not ok, it's the users problem.
-        
-        return a
-    
-    # Pointer
-    if "PTR" in p:
-        p.replace("PTR", "", 1) # Hope not
-        p.replace("FAR", "", 1)
-        return Label(p, include_segment=True)
-
-    raise Exception(f"Nebylo možné zpracovat parametr {p}")
-
-def parse_number(s: str) -> int:
-    if s[-1] == "h":
-        return int(s[:-1], 16)
-    if s[-1] == "b":
-        return int(s[:-1], 2)
-    return int(s)
-
-
 def parse_next_instruction(segment, IP): # Disassembler
     instruction = Instruction()
     span = 0
@@ -128,6 +76,58 @@ def parse_next_instruction(segment, IP): # Disassembler
                 raise Exception("Unrecognised instruction property")
 
     return instruction
+
+
+def parse_param(p: str) -> 'Parameter': # Part of assembler
+    """
+    ! Work in progress. Yet can't handle labels nor math (MOV [label + 2], 42)
+    ? Perhaps regex would be usefull??
+    pls help
+    """
+    # Register
+    if p in REGISTERS:
+        return Register(p)
+
+    # Immutable
+    try:
+        print(parse_number(p))
+        return Immutable(parse_number(p))
+    except:
+        pass
+
+    # Memmory
+    if p[0] == '[' and p[-1] == "]":
+        p = p[1:-1]
+        a = Memmory(None, None)
+        for r in MOD_00_RM:
+            if r in p:
+                a.source = r
+                p = p.replace(r + '+', "+")
+                break
+        else:
+            raise Exception("Nebyl rozpoznán segment")
+        
+        if p[0] == "+":
+            p = p.replace('+', '')
+            a.displacement = parse_number(p) # If is not ok, it's the users problem.
+        
+        return a
+    
+    # Pointer
+    if "PTR" in p:
+        p.replace("PTR", "", 1) # Hope not
+        p.replace("FAR", "", 1)
+        return Label(p, include_segment=True)
+
+    raise Exception(f"Nebylo možné zpracovat parametr {p}")
+
+def parse_number(s: str) -> int:
+    if s[-1] == "h":
+        return int(s[:-1], 16)
+    if s[-1] == "b":
+        return int(s[:-1], 2)
+    return int(s)
+
 
 
 RM_8_REGS = ["AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"]
