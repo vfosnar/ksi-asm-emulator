d9e1e8d1c1398bf731c670390b8eff83269c137a
Ondra N <ondran321@gmail.com>
2025-01-28 08:11:16 +0100

    Cleaned a bit

--- emulator.py
+++ emulator.py
@@ -1,17 +1,5 @@
 from disassembler import *
-from assembler import * # TODO: Tohle samozřejmě přepsat na jenom funkci assemble()
-
-class Byte:
-    def __init__(self, type, value):
-        self.type = "prefix"  # Prefix | instruction | data
-        self.value = "CS"
-
-        self.type = "instruction"
-        self.value = "MOV _, AX"
-
-        self.type = "data"
-        self.value = 125
-
+from assembler import assemble # TODO: Tohle samozřejmě přepsat na jenom funkci assemble()
 
 # Flags
 Flag = int
@@ -36,62 +24,50 @@ class Emulator:
 
             "SP": None,  # Stack pointer
 
-            "CS": None,  # Code segment
+            "CS": 0,  # Code segment
             "DS": None,  # Data segment
             "SS": None,  # Stack segment
             "ES": None,  # Extra segment
 
             "IP": 0,  # Instruction pointer
 
-            # Flags (zde se ukládají informace ohledně operací) TODO: Líp vysvětlit
-            "FI": 0
+            "FI": 0  # Flags register
         }
 
         self.program = program # Seznam bajtů
         self.running = True  # Může být uspáno pomocí instrukce HLT
 
+        self.instr_methods = {
+            "MOV": self.MOV, "ADD": self.ADD, "ADC": self.ADC,
+            "SUB": self.SUB, "SBB": self.SBB, "AND": self.AND,
+            "OR": self.OR,   "XOR": self.XOR, "NEG": self.NEG,
+            "NOP": self.NOP, "INC": self.INC, "DEC": self.DEC,
+            "HLT": self.HLT, "CMP": self.CMP, "TEST": self.TEST,
+        }
+
     def run(self):
         while self.running:
+            # TODO: nechci to dát dovnitř třídy??
             instr, span = parse_next_instruction(
-                self.program, self.registers["IP"])
+                self.program, 
+                self.get_register("CS") + self.registers["IP"]
+            )
             self.registers["IP"] += span
-            match instr.operation:
-                case "MOV":
-                    self.MOV(instr)
-                case "ADD":
-                    self.ADD(instr)
-                # TODO Donplnit zbyle instrukce
 
-                case "AND":
-                    self.AND(instr)
+            if instr.operation not in self.instr_methods:
+                raise Exception(f"This emulator doesn't support this operation: {instr.operation}")
+            
+            self.instr_methods[instr.operation](instr)
 
-                case "OR":
-                    self.OR(instr)
-                case "XOR":
-                    self.XOR(instr)
-                
-                case "NEG":
-                    self.NEG(instr)
+    def get_address(self, arg: Memmory):
+        segment = self.get_register(arg.segment)
+        offset += arg.displacement
 
+        for reg in arg.source.split("+"):
+            offset += self.get_register(reg)
 
-                case "NOP":
-                    self.NOP(instr)
-                case "HLT":
-                    self.HLT()
-                case unknown:
-                    raise Exception(f"This emulator doesn't support this operation: {unknown}")
-
-    def set_flag(self, flag: Flag, val: bool):
-        f_reg = self.registers["FI"]
-        f_reg = f_reg & ~(1 << flag)  # Make the flag 0
-
-        if val:
-            f_reg = f_reg | (1 << flag)
-
-    def get_flag(self, flag: Flag):
-        f_reg = self.registers["FI"]
-        return (f_reg // (2**flag)) % 2
-
+        return segment + offset
+    
     def get_register(self, reg: str):
         reg = reg.upper()
         assert reg in REGISTERS, f"There is no \"{reg}\" register in this emulator."
@@ -105,16 +81,9 @@ class Emulator:
         assert output is not None, f"Trying to get value of register {reg} with undefined value."
         return output
 
-    def get_address(self, arg: Memmory):
-        segment = self.get_register(arg.segment)
-        offset += arg.displacement
-
-        for reg in arg.source.split("+"):
-            offset += self.get_register(reg)
-
-        return segment + offset
-
     def set_register(self, reg: str, val: int):
+        assert val >= 0, f"Do funkce set_register vkládejte hodnotu v přímém kódu, jako nezáporné číslo."
+
         if reg[1] == "X":
             assert 0 <= val <= 2**16, \
                 f"Snažíte se do registru {reg} vložit hodnotu {val}, která je mimo rozsah."
@@ -139,7 +108,6 @@ class Emulator:
         seg = self.get_register(segment)
         self.program[seg + offset] = value
 
-    # Autorská pomocná funkce
     def get_value(self, arg: Parameter):
         # Asi autorská pomocná funkce. Ať si to kdyžtak udělají sami.
         output = None
@@ -159,7 +127,6 @@ class Emulator:
 
         return output
 
-    # Autorská pomocná funkce
     def set_value(self, arg, val, size):
         match arg:
             case Register():
@@ -174,6 +141,18 @@ class Emulator:
                 for i in range(size // 8):
                     self.set_byte(arg.segment, offset + i, val % 2**8)
                     val //= 2**8
+
+    # ------- FLAGS: --------
+    def set_flag(self, flag: Flag, val: bool):
+        f_reg = self.registers["FI"]
+        f_reg = f_reg & ~(1 << flag)  # Make the flag 0
+
+        if val:
+            f_reg = f_reg | (1 << flag)
+
+    def get_flag(self, flag: Flag):
+        f_reg = self.registers["FI"]
+        return (f_reg // (2**flag)) % 2
 
     def set_pf(self, result):
         counter = 0
@@ -199,37 +178,13 @@ class Emulator:
         overflow = not (2**(8*opsize - 1) <= result < 2**(8*opsize))
         self.set_flag(OF, overflow)
 
-    def set_flags(self, result: int, opsize: int, flags: list[Flag]):
-        for flag in flags:
-            if flag not in [OF, CF, ZF, PF, SF]:
-                raise Exception(f"Flag {flag} is not supported.")
-            self.set_flag(flag, False)
-        
-        # result is in twos complement
-        if OF in flags and not (2**(opsize - 1) <= result < 2**opsize):
-            self.set_flag(OF, True)
-        
-        if CF in flags:
-            is_carry = not (2**(8*opsize) <= result < 2**(8*opsize + 1))
-            self.set_flag(CF, is_carry)
-
-        if ZF in flags:
-            self.set_flag(ZF, result == 0)
-
-        # Need binary number
-        result = to_twos_complement(result, opsize)
-
-        if PF in flags:
-            counter = 0
-            for _ in range(8):
-                counter += result % 2
-                result //= 2
-            self.set_flag(PF, counter % 2 == 0)
-        
-        if SF in flags:
-            sign = result // 2**(8*opsize - 1)
-            self.set_flag(SF, sign == 1)
-        
+    def set_state_flags(self, result: int, opsize: int):
+        """Sets the ZF, PF and SF flag."""
+        self.set_zf(result)
+        self.set_pf(result)
+        self.set_sf(result, opsize)    
+    
+    # ------- INSTRUCTIONS: --------
     def MOV(self, instruction: Instruction):
         assert len(instruction.arguments) == 2
         arg1, arg2 = instruction.arguments
@@ -238,6 +193,7 @@ class Emulator:
         self.set_value(arg1, to_insert, instruction.size)
         # MOV Nenastavouje příznaky
 
+    # ------- ARITMETIC INSTRUCTION: --------
     def ADD(self, instruction):
         # TODO: Dodělat znaménkové přetečení
         val1 = self.get_value(instruction.arguments[0])
@@ -250,7 +206,7 @@ class Emulator:
 
         self.set_value(instruction.arguments[0], vysledek, instruction.size)
 
-        self.set_flags(vysledek, instruction.size, [OF, CF, ZF, PF, SF])
+        self.set_state_flags(vysledek, instruction.size, [OF, CF, ZF, PF, SF])
 
         self.set_cf(vysledek, instruction.size)
         self.set_sf(vysledek, instruction.size)
@@ -260,13 +216,27 @@ class Emulator:
         ...
 
     def ADC(self, instruction):
-        pass
+        val1 = self.get_value(instruction.arguments[0])
+        val2 = self.get_value(instruction.arguments[1])
+        cf = 1 if self.get_flag(CF) else 0 
+        vysledek = val1 + val2 + cf 
+        self.set_value(instruction.arguments[0], vysledek % 2**(instruction.size), instruction.size)
+        # TODO: Dodělat flags
 
     def SUB(self, instruction):
-        pass
+        val1 = self.get_value(instruction.arguments[0])
+        val2 = self.get_value(instruction.arguments[1]) 
+        result = val1 - val2
+        self.set_value(instruction.arguments[0], result % 2**(instruction.size), instruction.size)
+        # TODO: Dodělat flags
 
     def SBB(self, instruction):
-        pass
+        val1 = self.get_value(instruction.arguments[0])
+        val2 = self.get_value(instruction.arguments[1])
+        cf = 1 if self.get_flag(CF) else 0
+        result = val1 - val2 - cf
+        self.set_value(instruction.arguments[0], result % 2**(instruction.size), instruction.size)
+        # TODO: Dodělat flags
 
     def INC(self, instruction):
         val = self.get_value(instruction.arguments[0]) + 1
@@ -279,26 +249,29 @@ class Emulator:
         self.set_flag(OF, val == 0)  # TODO: Tohle se mi nějak nezdá
 
     def DEC(self, instruction):
-        pass
+        val = self.get_value(instruction.arguments[0]) - 1
+        val %= 2**(8*instruction.size)
+        self.set_value(instruction.arguments[0], val, instruction.size)
 
     def NEG(self, instruction):
         val = self.get_value(instruction.arguments[0])
         val = ~val + 1
-        self.set_value(instruction.arguments[0], val, instruction.size)
+        self.set_value(instruction.arguments[0], val % 2**instruction.size, instruction.size)
         # TODO: doplnit flags
 
-    def CMP(self, instruction):
+    def MUL(self, instruction):
         pass
 
-    def CBW(self, instruction):
+    def IMUL(self, instruction):
         pass
 
-    # TODO: Chceme i CBD??
-    # (Convert Byte to Doubleword)
+    def DIV(self, instruction):
+        pass
 
-    # ? Jak implementovat tyto??
-    # MUL, IMUL, DIV, IDIV - doufám, že ne
+    def IDIV(self, instruction):
+        pass
 
+    # ------- LOGICKÉ INSTRUKCE: --------
     def AND(self, instruction):
         val1 = self.get_value(instruction.arguments[0])
         val2 = self.get_value(instruction.arguments[1])
@@ -338,6 +311,17 @@ class Emulator:
         self.set_value(instruction.arguments[0], result, instruction.size)
         # Nemění příznaky
 
+    def CBW(self, instruction): 
+        pass
+
+    # ------- POROVNÁVACÍ  INSTRUKCE: --------
+    def CMP(self, instruction):
+        val1 = self.get_value(instruction.arguments[0])
+        val2 = self.get_value(instruction.arguments[1])
+        result = val1 - val2
+
+        self.set_state_flags(result, instruction.size)
+
     def TEST(self, instruction):
         # And, akorát se neukládá. Jen nastavuje příznaky (flags)
         val1 = self.get_value(instruction.arguments[0])
@@ -348,9 +332,13 @@ class Emulator:
         self.set_zf(result)
         self.set_pf(result)
 
+    # ------- DALŠÍ INSTRUKCE: --------
     def NOP(self, instruction):
-        # Already implemented for you ;-)
+        pass  # Already implemented ;-)
+
+    def CBW(self, instruction):
         pass
+
 
     # ROL, ROR, RCR, RCL - Rotate
     # SAL, SHL, SAR, SHR - Shift
@@ -358,7 +346,6 @@ class Emulator:
     # JUMP - Zvláštní úloha
     # JMP
     # JZ, JNZ, ... - Jumpy
-    # !? Krátký skok, dlouhý skok - to budou taky implementovat??
 
     # STACK - Zvláštní úloha
     # PUSH, POP
@@ -369,7 +356,7 @@ class Emulator:
     # INTERUPTIONS - Zvláštní úloha
     # INT, IRET
 
-    def HLT(self):
+    def HLT(self, instruction):
         # Už jsme ji pro Vás naprogramovali ;-)
         self.running = False
 
@@ -393,6 +380,7 @@ segment code
         MOV AL, 11111111b
         MOV AH, 10011001b
         AND AL, AH
+        MOV AX, 7
         NEG AX
         HLT
 
@@ -408,6 +396,25 @@ segment data
 n       db 42
 x       resb 4
 y       db 0ABh
+"""
+
+    test_code = """
+segment code
+        MOV BX, data    ; Nastaví datový segment. Není podstatné
+        MOV DS, BX      
+
+        MOV AX, 42
+        SUB AX, [a]
+
+        MOV BX, 15
+        AND BX, 10101010b  ; b na konci označuje binární zápis čísla
+        INC BX
+        MOV [a], BX
+
+        HLT        
+
+segment data
+a       db 12
 """
 
     program = assemble(code3)
